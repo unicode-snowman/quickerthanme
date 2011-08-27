19c900cd0a47cfa900f15b0adb3c4926ca78aec7
Daniel Lindsley <daniel@toastdriven.com>
2011-08-27 01:49:10 -0500

    Mo views, mo problems.

--- utils/wrappers.js
+++ utils/wrappers.js
@@ -1,3 +1,4 @@
+var render    = require('../lib/templates')
 
 exports.decorate_auth = function(fn) {
   var User = require('../lib/models').models.User
@@ -11,8 +12,8 @@ exports.decorate_auth = function(fn) {
       User.objects.get({access_token:req.session.auth.github.accessToken})(function(err, data) {
         if(!err)
           req.user = data
-        fn.apply(self, args) 
-      }) 
+        fn.apply(self, args)
+      })
     } else {
       fn.apply(self, args)
     }
@@ -30,10 +31,26 @@ exports.require_login = function(fn) {
 
     var args = [].slice.call(arguments)
       , self = this
- 
+
     if(req.user)
       fn.apply(self, args)
     else
       redirect()
   })
+}
+
+exports.require_race = function(fn) {
+  var Race = require('../lib/models').models.Race
+
+  return function(req, res, race_pk) {
+    var args = [].slice.call(arguments)
+        , self = this
+
+    Race.objects.get({"pk": race_pk}).on('data', function(race) {
+      args.splice(2, 1, race)
+      fn.apply(self, args)
+    }).on('error', function(err) {
+      render('404.html', {}, req, resp, 404)
+    })
+  }
 }
