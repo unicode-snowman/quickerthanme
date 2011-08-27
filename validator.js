91ee6b3ce9a45f57133da4569c8e7fbe4e596a3e
Chris Dickinson <christopher.s.dickinson@gmail.com>
2011-08-27 16:06:27 -0500

    Add validator

---
+++ validator.js
@@ -0,0 +1,19 @@
+var vm = require('vm')
+
+process.stdin.on('data', function(data) {
+  data = data.toString('utf8')
+  var packet  = JSON.parse(data)
+    , input   = packet.input
+    , solution= packet.solution
+
+  solution = '(function() { return '+solution+' })()'
+  var result = vm.runInNewContext(solution, {'input':JSON.parse(input)}, 'test')
+  result = JSON.stringify(result)
+
+  // give this bitch a drain event. bitches love drain events.
+  if(!process.stdout.write(result)) {
+    process.stdout.on('drain', process.exit.bind(process))
+  }
+})
+
+process.stdin.resume()
