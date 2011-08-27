77bf2ddc21e2a912f6e6ec2f7bd9e11a32e1a8da
Daniel Lindsley <daniel@toastdriven.com>
2011-08-26 19:23:46 -0500

    Added NKO reference app.js.

---
+++ app.js
@@ -0,0 +1,10 @@
+app.listen(process.env.NODE_ENV === 'production' ? 80 : 8000, function() {
+  console.log('Ready');
+
+  // if run as root, downgrade to the owner of this file
+  if (process.getuid() === 0)
+    require('fs').stat(__filename, function(err, stats) {
+      if (err) return console.log(err)
+      process.setuid(stats.uid);
+    });
+});
